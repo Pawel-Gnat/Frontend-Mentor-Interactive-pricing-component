e712248d4da6d49d88be54b749bca84947cfe249
Paweł <pawel_gnat@o2.pl>
2022-08-05 22:12:27 +0200

    working with js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
-// pageviews
-// price
-// toggle
-
 const pricePerViewsData = [8, 12, 16, 24, 36]
 const pageviewsPerPriceData = ['10K', '50K', '100K', '500K', '1M']
 const priceRangeSlider = document.querySelector('#priceRange')
-const togglePricePerPeriod = document.querySelector('#priceSwitch')
+const changePriceInput = document.querySelector('#priceSwitch')
 const TextareaPrice = document.querySelector('.slidecontainer__price')
 const TextareaPageviews = document.querySelector('.slidecontainer__pageviews')
 const windowMediaQuery = window.matchMedia('(min-width: 505px)')
-
-priceRangeSlider.oninput = function () {
-	TextareaPrice.textContent = '$' + pricePerViewsData[this.value].toFixed(2)
-}
-priceRangeSlider.oninput()
-
-function showPageviewsPerPrice() {
-	for (let i = 0; pricePerViewsData.length; i++) {
-		// 	TextareaPageviews.textContent = pageviewsPerPriceData[i]
-
-		switch (changePageviews) {
-			case 1:
-				TextareaPageviews.textContent[i]
-				break
-		}
-	}
-}
-showPageviewsPerPrice()
 
 function addDiscountText(e) {
 	if (e.matches) {
@@ -37,3 +15,41 @@ function addDiscountText(e) {
 }
 addDiscountText(windowMediaQuery)
 windowMediaQuery.addListener(addDiscountText)
+
+priceRangeSlider.oninput = function () {
+	TextareaPrice.textContent = '$' + pricePerViewsData[this.value].toFixed(2)
+	TextareaPageviews.textContent = pageviewsPerPriceData[this.value] + ' pageviews'
+}
+priceRangeSlider.oninput()
+
+function changePeriodPrice() {
+	if (changePriceInput.checked == true) {
+		document.querySelector('.slidecontainer__price--period').textContent = '/ year'
+		// setYearPrice()
+	} else {
+		document.querySelector('.slidecontainer__price--period').textContent = '/ month'
+	}
+}
+changePeriodPrice()
+
+function setYearPrice() {
+	const discount = 0.75
+
+	for (let i = 0; i < pricePerViewsData.length; i++) {
+		pricePerViewsDataYear[i] = pricePerViewsData[i] * discount * 12
+	}
+}
+
+changePriceInput.addEventListener('click', e => {
+	const inputChecked = changePriceInput.checked
+
+	switch (inputChecked) {
+		case true:
+			document.querySelector('.slidecontainer__price--period').textContent = '/ year'
+			// setYearPrice()
+			break
+		case false:
+			document.querySelector('.slidecontainer__price--period').textContent = '/ month'
+			break
+	}
+})
